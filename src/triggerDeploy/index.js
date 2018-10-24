adf395cecfc657d66421894cafd43c335d29422f
Anna Spysz <anna.spysz@stackery.io>
2018-10-24 16:51:47 -0700

    testing

--- src/triggerDeploy/index.js
+++ src/triggerDeploy/index.js
@@ -1,5 +1,16 @@
-exports.handler = async message => {
+exports.handler = async (req, ctx) => {
   console.log(message);
 
-  return {};
+  // detect change in react-frontend repo master
+
+  // trigger react-webhook-test/src/deployFrontEnd/index.js to run
+
+  // how? deploy script? can it be done though API?
+
+
+  console.log(`Got event ${req.headers['X-GitHub-Event']}`);
+  return {
+      statusCode: 200,
+      body: ""
+  }
 }
