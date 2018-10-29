4854feade98f82058316befe11213d45767c4ebd
Anna Spysz <anna.spysz@stackery.io>
2018-10-29 14:19:04 -0700

    testing

--- src/triggerDeploy/index.js
+++ src/triggerDeploy/index.js
@@ -1,11 +1,13 @@
-exports.handler = (event, context, callback) => {
+module.exports.githubWebhookListener = (event, context, callback) => {
   let errMsg; // eslint-disable-line
   const token = process.env.GITHUB_WEBHOOK_SECRET;
   const headers = event.headers;
-  const sig = headers['X-Hub-Signature'];
+  const body = JSON.parse(event.body);
+  const { repository, pusher, sender } = body;
+  const repo = repository.name;
+  const username = pusher.name;
+  const url = repository.url;
   const githubEvent = headers['X-GitHub-Event'];
-  const id = headers['X-GitHub-Delivery'];
-  const calculatedSig = signRequestBody(token, event.body);
 
   if (typeof token !== 'string') {
     errMsg = 'Must provide a \'GITHUB_WEBHOOK_SECRET\' env variable';
@@ -16,55 +18,11 @@ exports.handler = (event, context, callback) => {
     });
   }
 
-  if (!sig) {
-    errMsg = 'No X-Hub-Signature found on request';
-    return callback(null, {
-      statusCode: 401,
-      headers: { 'Content-Type': 'text/plain' },
-      body: errMsg,
-    });
-  }
-
-  if (!githubEvent) {
-    errMsg = 'No X-Github-Event found on request';
-    return callback(null, {
-      statusCode: 422,
-      headers: { 'Content-Type': 'text/plain' },
-      body: errMsg,
-    });
-  }
-
-  if (!id) {
-    errMsg = 'No X-Github-Delivery found on request';
-    return callback(null, {
-      statusCode: 401,
-      headers: { 'Content-Type': 'text/plain' },
-      body: errMsg,
-    });
-  }
-
-  if (sig !== calculatedSig) {
-    errMsg = 'X-Hub-Signature incorrect. Github webhook token doesn\'t match';
-    return callback(null, {
-      statusCode: 401,
-      headers: { 'Content-Type': 'text/plain' },
-      body: errMsg,
-    });
-  }
-
-  /* eslint-disable */
-  console.log('Oh hai, I pushed a thing!');
   console.log('---------------------------------');
-  console.log('Payload', event.body);
+  console.log(`Github-Event: "${githubEvent}" on this repo: "${repo}" at the url: ${url}.\n ${username} is to blame if something was broken.`);
   console.log('---------------------------------');
-  console.log(`Github-Event: "${githubEvent}" on this repo: "${event.body.repository.html_url}" - "${event.body.sender.login}" is to blame for all our woes.`);
+  console.log(event.body);
   console.log('---------------------------------');
-  console.log('Some wisdom');
-  console.log(event.body.zen);
-  /* eslint-enable */
-
-  // Do custom stuff here with github event data
-  // For more on events see https://developer.github.com/v3/activity/events/types/
 
   const response = {
     statusCode: 200,
