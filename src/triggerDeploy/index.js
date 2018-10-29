7768be661dd0a836d894b29e2304645f2a18c9e1
Anna Spysz <anna.spysz@stackery.io>
2018-10-29 12:08:51 -0700

    changing logs

--- src/triggerDeploy/index.js
+++ src/triggerDeploy/index.js
@@ -1,16 +1,77 @@
-exports.handler = async (req, ctx) => {
-  console.log(message);
+exports.handler = (event, context, callback) => {
+  let errMsg; // eslint-disable-line
+  const token = process.env.GITHUB_WEBHOOK_SECRET;
+  const headers = event.headers;
+  const sig = headers['X-Hub-Signature'];
+  const githubEvent = headers['X-GitHub-Event'];
+  const id = headers['X-GitHub-Delivery'];
+  const calculatedSig = signRequestBody(token, event.body);
 
-  // detect change in react-frontend repo master
-
-  // trigger react-webhook-test/src/deployFrontEnd/index.js to run
-
-  // how? deploy script? can it be done though API?
-
-
-  console.log(`Got event ${req.headers['X-GitHub-Event']}`);
-  return {
-      statusCode: 200,
-      body: ""
+  if (typeof token !== 'string') {
+    errMsg = 'Must provide a \'GITHUB_WEBHOOK_SECRET\' env variable';
+    return callback(null, {
+      statusCode: 401,
+      headers: { 'Content-Type': 'text/plain' },
+      body: errMsg,
+    });
   }
-}
+
+  if (!sig) {
+    errMsg = 'No X-Hub-Signature found on request';
+    return callback(null, {
+      statusCode: 401,
+      headers: { 'Content-Type': 'text/plain' },
+      body: errMsg,
+    });
+  }
+
+  if (!githubEvent) {
+    errMsg = 'No X-Github-Event found on request';
+    return callback(null, {
+      statusCode: 422,
+      headers: { 'Content-Type': 'text/plain' },
+      body: errMsg,
+    });
+  }
+
+  if (!id) {
+    errMsg = 'No X-Github-Delivery found on request';
+    return callback(null, {
+      statusCode: 401,
+      headers: { 'Content-Type': 'text/plain' },
+      body: errMsg,
+    });
+  }
+
+  if (sig !== calculatedSig) {
+    errMsg = 'X-Hub-Signature incorrect. Github webhook token doesn\'t match';
+    return callback(null, {
+      statusCode: 401,
+      headers: { 'Content-Type': 'text/plain' },
+      body: errMsg,
+    });
+  }
+
+  /* eslint-disable */
+  console.log('Oh hai, I pushed a thing!');
+  console.log('---------------------------------');
+  console.log('Payload', event.body);
+  console.log('---------------------------------');
+  console.log(`Github-Event: "${githubEvent}" on this repo: "${event.body.repository.html_url}" - "${event.body.sender.login}" is to blame for all our woes.`);
+  console.log('---------------------------------');
+  console.log('Some wisdom');
+  console.log(event.body.zen);
+  /* eslint-enable */
+
+  // Do custom stuff here with github event data
+  // For more on events see https://developer.github.com/v3/activity/events/types/
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      input: event,
+    }),
+  };
+
+  return callback(null, response);
+};
